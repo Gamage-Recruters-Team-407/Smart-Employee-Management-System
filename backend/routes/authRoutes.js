1d32984bbe822e8340f14f3f5fea0a485ac2594b
Dilhara-Jayashan <140555849+Dilhara-Jayashan@users.noreply.github.com>
2026-05-22 11:34:07 +0530

    added middleware ,user model bug fix,added authrouter to service

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -8,3 +8,9 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 
 export default router;
+
+
+
+
+//router.get('/all', protect, authorizeRoles('Admin', 'HR'), getAllEmployees);  
+// example of protected route with role-based access control (Admin and HR can access)
